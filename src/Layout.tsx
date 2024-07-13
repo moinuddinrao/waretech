68305112eb1d8288123bfef7b19bf65e77c44074
moinuddinrao <moinrajput594@gmail.com>
2024-07-13 19:33:50 +0500

    feat: create Layout component for overall page layout

---
+++ src/Layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { Layout as AntdLayout, Space } from "antd";
+
+import { Sidebar } from "@/components/Sidebar";
+import { Header } from "@/components/Header";
+
+export const Layout = () => {
+  return (
+    <Space direction="vertical" className="w-full h-full min-h-screen">
+      <AntdLayout>
+        <Sidebar />
+        <AntdLayout className="w-full h-full bg-light px-[2.5vw]">
+          <Header />
+          <AntdLayout.Content className="w-full h-full min-h-[90vh] flex flex-col gap-y-10 pb-10">
+            <Outlet />
+          </AntdLayout.Content>
+        </AntdLayout>
+      </AntdLayout>
+    </Space>
+  );
+};
