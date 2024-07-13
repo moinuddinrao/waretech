d4e8db5188534a4aee1a3c8346d9ce74875a3543
moinuddinrao <moinrajput594@gmail.com>
2024-07-13 19:09:22 +0500

    feat: add routes for navigation

---
+++ src/common/routes.ts
@@ -0,0 +1,19 @@
+export const ROUTES = {
+  profile: "/profile",
+  settings: "/settings",
+
+  dashboards: "/dashboards",
+  maintenanceMenu: "/dashboards/maintenance-menu",
+  productManagement: "/dashboards/product-management",
+  purchasing: "/purchasing",
+  reporting: "/reporting",
+  eqsi: "/eqsi",
+
+  customers: "/customers",
+  suppliers: "/suppliers",
+  userRoles: "/user-roles",
+
+  aiTool: "/ai-tool",
+  eqsLink: "/eqs-link",
+  anOther: "/an-other",
+} as const;
