80443193432314ebde48b8c154482874088154b5
moinuddinrao <moinrajput594@gmail.com>
2024-07-13 19:12:06 +0500

    feat: add styles for headings, labels, lines, sections, and boxes

---
+++ src/styles/index.ts
@@ -0,0 +1,13 @@
+export const styles = {
+  heading1: "m-0 p-0 font-lato font-bold text-[48px] leading-[56px]",
+  heading2: "m-0 p-0 font-lato font-bold text-[32px] leading-[40px]",
+  heading3: "m-0 p-0 font-lato font-bold text-[24px] leading-[32px]",
+  heading4: "m-0 p-0 font-lato font-semibold text-[20px] leading-[28px]",
+  heading5: "m-0 p-0 font-lato font-bold text-[16px] leading-[24px]",
+
+  label: "m-0 p-0 font-lato font-medium text-gray",
+  line: "w-full h-[2px] text-white",
+
+  section: "flex flex-col gap-10",
+  box: "flex flex-col gap-10 p-10 bg-white shadow-lg",
+};
