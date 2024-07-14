68b0e3f9023cc3803ad5f948df66514a65ae5ea1
moinuddinrao <moinrajput594@gmail.com>
2024-07-15 00:18:33 +0500

    refactor: update App component to use React Query for API queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { App as AntdApp, ConfigProvider } from "antd";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "@/utils/react-query.service";
+
 import { Router } from "./Router";
 
 function App() {
   return (
-    <AntdApp>
-      <ConfigProvider
-        theme={{
-          token: {
-            borderRadius: 0,
-          },
-        }}
-      >
-        <Router />
-      </ConfigProvider>
-    </AntdApp>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <AntdApp>
+        <ConfigProvider
+          theme={{
+            token: {
+              borderRadius: 0,
+            },
+          }}
+        >
+          <Router />
+        </ConfigProvider>
+      </AntdApp>
+    </QueryClientProvider>
   );
 }
 
