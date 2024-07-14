fd127f59f23466486f43ea2dd48f3fa0c43dfc72
moinuddinrao <moinrajput594@gmail.com>
2024-07-14 14:58:15 +0500

    style: update PrimaryButton component styles

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -6,7 +6,7 @@ const PrimaryButton = (props: ButtonProps) => {
       {...props}
       htmlType={props?.htmlType}
       onClick={props?.onClick}
-      className={`w-fit font-medium px-8 py-4 !border-none !bg-[#3A5BFF] hover:!bg-[#324cca] !text-white rounded-lg ${props?.className}`}
+      className={`w-fit !border-none !bg-[#3A5BFF] hover:!bg-[#324cca] !text-white hover:!text-white rounded-md shadow-none ${props?.className}`}
     >
       {props.children}
     </Button>
