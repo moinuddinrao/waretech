c3fc6c3f230c2b419f15aff3f7f212538b3fc418
moinuddinrao <moinrajput594@gmail.com>
2024-07-14 18:33:53 +0500

    style: fix hover background color in PrimaryButton component

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -6,7 +6,7 @@ const PrimaryButton = (props: ButtonProps) => {
       {...props}
       htmlType={props?.htmlType}
       onClick={props?.onClick}
-      className={`w-fit !border-none !bg-[#3A5BFF] hover:!bg-[#324cca] !text-white hover:!text-white rounded-md shadow-none ${props?.className}`}
+      className={`w-fit !border-none !bg-[#3A5BFF] hover:!bg-[#324cca] text-white hover:!text-white rounded-md shadow-none ${props?.className}`}
     >
       {props.children}
     </Button>
