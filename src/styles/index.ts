366a6c708f19b54d225c10f3cfa164257c407907
moinuddinrao <moinrajput594@gmail.com>
2024-07-14 15:03:51 +0500

    style: update heading styles in index.ts

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,13 +1,14 @@
 export const styles = {
-  heading1: "m-0 p-0 font-lato font-bold text-[48px] leading-[56px]",
-  heading2: "m-0 p-0 font-lato font-bold text-[32px] leading-[40px]",
-  heading3: "m-0 p-0 font-lato font-bold text-[24px] leading-[32px]",
-  heading4: "m-0 p-0 font-lato font-semibold text-[20px] leading-[28px]",
-  heading5: "m-0 p-0 font-lato font-bold text-[16px] leading-[24px]",
+  heading1: "m-0 p-0 font-lato font-bold text-[24px] leading-[28px]",
+  heading2: "m-0 p-0 font-lato font-bold text-[22px] leading-[26px]",
+  heading3: "m-0 p-0 font-lato font-semibold text-[20px] leading-[24px]",
+  heading4: "m-0 p-0 font-lato font-semibold text-[18px] leading-[22px]",
+  heading5: "m-0 p-0 font-lato font-semibold text-[16px] leading-[20px]",
+  heading6: "m-0 p-0 font-lato text-[14px] leading-[18px]",
 
   label: "m-0 p-0 font-lato font-medium text-gray",
   line: "w-full h-[2px] text-white",
 
-  section: "flex flex-col gap-10",
-  box: "flex flex-col gap-10 p-10 bg-white shadow-lg",
+  section: "flex flex-col gap-5",
+  box: "flex flex-col gap-5 p-5 bg-white shadow-lg",
 };
